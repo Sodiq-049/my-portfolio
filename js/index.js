2a9f0a35793cc59f6f05b4a75207eb532d562772
Sodiq-049 <sodiqogunmola049@gmail.com>
2024-11-18 02:44:59 +0100

    updated code

--- js/index.js
+++ js/index.js
@@ -73,6 +73,10 @@ navTogglerBtn.addEventListener('click', () => {
     asideSectionTogglerBtn();
 });
 
+navTogglerBtn.addEventListener('touchstart', () => {
+    asideSectionTogglerBtn();
+});
+
 function asideSectionTogglerBtn(){
     aside.classList.toggle('open');
     navTogglerBtn.classList.toggle('open');
